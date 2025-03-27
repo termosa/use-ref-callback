1e36c7fd4ee002ef15702d742d6b6eb5738f6be5
Stanislav Termosa <termosa.stanislav@gmail.com>
2025-03-26 23:32:49 -0700

    Support React 19

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 
-export function useRefCallback<T extends undefined |((...args: any[]) => any)>(callback: T): T {
+export function useRefCallback<T extends undefined | ((...args: any[]) => any)>(callback: T): T {
   const ref = React.useRef<T>(callback)
   ref.current = callback
-  const memoized = React.useCallback(((...args) => ref.current?.(...args)), []);
-  return ref.current && memoized as T;
-};
+  const memoized = React.useCallback(
+    (...args: Parameters<T extends (...args: any[]) => any ? T : never>) =>
+      ref.current?.(...args),
+    []
+  )
+  return ref.current && (memoized as T)
+}
 
 export default useRefCallback
